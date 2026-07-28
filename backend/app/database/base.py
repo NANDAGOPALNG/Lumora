f7858162d7a96ff91d00d233041ec481d5075c6b
NANDAGOPALNG <nandagopalng2004@gmail.com>
2026-07-28 15:37:01 +0530

    Sucessfully tested the core Database models

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -2,5 +2,3 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-# Import models to register them with Base.metadata
-from app.models import User, Workspace, Document
